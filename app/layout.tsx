1e2f57004b3736b52acfb461e0eab06d5340c24d
cxllinheenan <collinpheenan@gmail.com>
2025-01-07 11:56:49 -0500

    updated emoji favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,12 @@ export const metadata: Metadata = {
     default: 'Collin Heenan',
     template: '%s | Collin Heenan',
   },
+  icons: {
+    icon: [{
+      url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🧪</text></svg>',
+      href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🧪</text></svg>'
+    }]
+  },
   description: 'This is my portfolio.',
   openGraph: {
     title: 'My Portfolio',
