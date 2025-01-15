5848a7e2873042c084b96560be8e91cc93c85f66
cxllinheenan <collinpheenan@gmail.com>
2025-01-15 11:59:17 -0500

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
         Collin Heenan
       </h1>
-      <p className="mb-4">
+      <p className="mb-8">
         {`Undergraduate student at Worcester State University studying Economics & Computer Science.`}
       </p>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-2 text-1xl font-semibold tracking-tighter">
         Academic Focus
       </h1>
       <p className="mb-4">
@@ -19,6 +19,12 @@ export default function Page() {
         Statistical Analysis,
         Software Engineering,
         System Design.`} 
+      </p>
+      <h1 className="mb-2 text-1xl font-semibold tracking-tighter">
+        Currently
+      </h1>
+      <p className="mb-4">
+      {`Interning at Cornerstone Bank; Working on Commercial Data.`} 
       </p>
       <div className="my-8">
         <BlogPosts/>
